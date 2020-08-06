78c242ab37c1f28b92d886e82d7861caae0ed3b2
davidnaples <33699327+davidnaples@users.noreply.github.com>
2020-08-06 12:34:22 +0200

    ye

--- transcriptphp/index.php
+++ transcriptphp/index.php
@@ -1,8 +1,27 @@
 <?php
-define('SALUTARE', 'Ciaobello');
-echo SALUTARE;
-$var1 = 1;
-echo $var1;
+const player1 = 'Bobby'; 
+const player2 = 'Bybbo';
+$player1Life=100;
+$player2Life=100;
 
 
+echo '/////////////////////////////////////';
+echo '  C\'est l\'heure du dueeeeeeeeel !';
+echo `/////////////////////////////////////`;
+echo '';
+
+while (player1Life > 0 && player2Life > 0) {
+    define( $puissanceAttaque1 = <math class="floor"></math> 
+    define( $puissanceAttaque2 = <math class="floor"></math>
+
+    console.log('');
+    console.log(`${player1} (${player1Life})  -  ${player2} (${player2Life})`);
+
+    console.log(`Bobby attaque et enlève ${puissanceAttaque1} points de vie`);
+    player2Life = player2Life - puissanceAttaque1;
+//et si on inverse ?
+    console.log(`Bybbo attaque et enlève ${puissanceAttaque2} points de vie`);
+    player1Life = player1Life - puissanceAttaque2;
+
+}
 ?>
