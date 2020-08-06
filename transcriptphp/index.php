dc202c961c7af1977ee9adba961a5f51ee3a1a67
davidnaples <33699327+davidnaples@users.noreply.github.com>
2020-08-06 15:04:08 +0200

    Update index.php

--- transcriptphp/index.php
+++ transcriptphp/index.php
@@ -1,27 +1,36 @@
 <?php
-const player1 = 'Bobby'; 
-const player2 = 'Bybbo';
+define("player1", "Bobby");
+define("player2" , "Bybbo"); 
 $player1Life=100;
 $player2Life=100;
 
 
 echo '/////////////////////////////////////';
 echo '  C\'est l\'heure du dueeeeeeeeel !';
-echo `/////////////////////////////////////`;
+echo '/////////////////////////////////////';
 echo '';
 
-while (player1Life > 0 && player2Life > 0) {
-    define( $puissanceAttaque1 = <math class="floor"></math> 
-    define( $puissanceAttaque2 = <math class="floor"></math>
+while ($player1Life > 0 && $player2Life > 0) {
+    define( "puissanceAttaque1" , rand (0 , 50));
+    define( "puissanceAttaque2" , rand(0 , 50));
 
-    console.log('');
-    console.log(`${player1} (${player1Life})  -  ${player2} (${player2Life})`);
+    echo('');
+    echo('player1' . $player1Life   'player2' . $player2Life);
 
-    console.log(`Bobby attaque et enlève ${puissanceAttaque1} points de vie`);
-    player2Life = player2Life - puissanceAttaque1;
-//et si on inverse ?
-    console.log(`Bybbo attaque et enlève ${puissanceAttaque2} points de vie`);
-    player1Life = player1Life - puissanceAttaque2;
+    echo('Bobby attaque et enlève puissanceAttaque1 points de vie');
+    $player2Life = $player2Life - puissanceAttaque1;
 
+    echo("Bybbo attaque et enlève ${puissanceAttaque2} points de vie");
+    $player1Life = $player1Life - puissanceAttaque2;
+}
+// retour du vainqueur   
+echo("/////////////////////////////////////");
+echo($player1Life > 0 ? `${player1} est vainqueur` : ``) ;
+echo($player2Life > 0 ? `${player2} est vainqueur` : ``) ;
+
+// player1 gagnant
+
+if ($player1Life > 0) {
+    echo("C'est un peu comme si c'est toi le joueur 1, alors bravo (même si tu n'as rien fait...).");
 }
 ?>
