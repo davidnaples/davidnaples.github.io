5e0c9902cf03b3a0464d4a502f50b43b6804bd5a
davidnaples <33699327+davidnaples@users.noreply.github.com>
2020-08-06 17:18:29 +0200

    ok

--- transcriptphp/index.php
+++ transcriptphp/index.php
@@ -14,23 +14,29 @@ while ($player1Life > 0 && $player2Life > 0) {
     define( "puissanceAttaque1" , rand (0 , 50));
     define( "puissanceAttaque2" , rand(0 , 50));
 
-    echo('');
-    echo('player1' . $player1Life   'player2' . $player2Life);
+    echo(puissanceAttaque1);
+    echo('player1' . ($player1Life) . 'player2' . ($player2Life));
 
-    echo('Bobby attaque et enlève puissanceAttaque1 points de vie');
+    echo('Bobby attaque et enlève puissanceAttaque1 points de vie\n');
     $player2Life = $player2Life - puissanceAttaque1;
 
-    echo("Bybbo attaque et enlève ${puissanceAttaque2} points de vie");
+    echo("Bybbo attaque et enlève ${puissanceAttaque2} points de vie\n");
     $player1Life = $player1Life - puissanceAttaque2;
 }
 // retour du vainqueur   
-echo("/////////////////////////////////////");
-echo($player1Life > 0 ? `${player1} est vainqueur` : ``) ;
-echo($player2Life > 0 ? `${player2} est vainqueur` : ``) ;
+echo("/////////////////////////////////////\n");
+if($player1Life > 0){ 
+    echo ( player1 . "est vainqueur :" ) ;
+}
+if($player2Life > 0){ 
+    echo  (player2 . "est vainqueur :" ) ;
+}
 
-// player1 gagnant
+ // a? alors b : sinon c;
+ //player1 gagnant
 
 if ($player1Life > 0) {
     echo("C'est un peu comme si c'est toi le joueur 1, alors bravo (même si tu n'as rien fait...).");
 }
+
 ?>
