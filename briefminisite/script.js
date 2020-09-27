9b27e92e491ac35e9face4f94e958fbddda1d2f1
davidnaples <33699327+davidnaples@users.noreply.github.com>
2020-09-27 17:54:01 +0200

    tableau

---
+++ briefminisite/script.js
@@ -0,0 +1,18 @@
+var tableau = [
+    { titre: "Imparare a Simplon1", auteur: "Stephen King", prix: "10500,00€" , image: src="briefminisite/assets/img/📘.png" ,resume:"blablablablabla"     },
+    { titre: "Imparare a Simplon2", auteur: "Stephen King", prix: "10500,00€" , image: img="briefminisite/assets/img/📘.png" ,resume:"blablablablabla"     },
+    { titre: "Imparare a Simplon3", auteur: "Stephen King", prix: "10500,00€" , image: img="briefminisite/assets/img/📘.png" ,resume:"blablablablabla"     },
+    { titre: "Imparare a Simplon4", auteur: "Stephen King", prix: "10500,00€" , image: img="briefminisite/assets/img/📘.png" ,resume:"blablablablabla"     },
+
+    ];
+/*function afficheRecommandations() {console.log tableau
+    
+}
+function afficheLivres(prixMax) {
+    
+}
+console.log(tableau)*/
+
+
+
+console.log(tableau);
